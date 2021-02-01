499cf4d030dd0610f9f4d70412ef36feac238021
lym1212 <47788697+lym1212@users.noreply.github.com>
2021-02-01 22:38:24 +0800

    Delete test directory

--- test/scripts/main.js
+++
@@ -1,9 +0,0 @@
-let myImage = document.querySelector('img');
-myImage.onclick = function(){
-	let mySrc = myImage.getAttribute('src');
-	if (mySrc === 'images/test.png'){
-		myImage.setAttribute('src', 'images/test2.png');
-	} else{
-		myImage.setAttribute('src', 'images/test.png');
-	}
-}
